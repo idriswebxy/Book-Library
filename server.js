d7b7ef21ba609da567532625f1f384515be3bf24
Idris <icoder112@gmail.com>
2019-06-20 18:16:39 -0400

    update

--- server.js
+++ server.js
@@ -18,7 +18,6 @@ const mongoose = require("mongoose");
 mongoose.connect(process.env.DB_URL, {
   useNewUrlParser: true
 });
-
 const db = mongoose.connection;
 db.on("error", error => console.log(error));
 db.once("open", () => console.log("Connected to Mongoose..."));
